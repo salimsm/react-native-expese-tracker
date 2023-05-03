02d7cd57f8b0cb0f4162d4c1c8f1bcc5a666de24
salimsm <salimmaharjansm2@gmail.com>
2023-05-03 21:58:34 +0545

    screen,storage,custom_widget folder created

---
+++ src/screen/add_screen.tsx
@@ -0,0 +1,10 @@
+import {StyleSheet, View} from 'react-native';
+import React from 'react';
+
+const ProfileScreen = () => {
+  return <View style={{}}></View>;
+};
+
+export default ProfileScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/screen/main_screen.tsx
@@ -0,0 +1,13 @@
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+import React from 'react';
+
+const MainScreen = () => {
+  return <View style={{flex: 1, padding: 8}}></View>;
+};
+
+export default MainScreen;
+
+const styles = StyleSheet.create({});
