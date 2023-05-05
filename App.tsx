6f5427cfaf0b9db371f04eca817736ae0f0c114a
salimsm <salimmaharjansm2@gmail.com>
2023-05-05 16:39:50 +0545

    login and register desing done

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet} from 'react-native';
 import MainScreen from './src/screen/main_screen';
 
@@ -6,17 +6,22 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import AddScreen from './src/screen/add_screen';
 import SplashScreen from 'react-native-splash-screen';
+import LoginScreen from './src/screen/login_screen';
+import RegisterScreen from './src/screen/register_screen';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  useEffect(()=>{
+  useEffect(() => {
     SplashScreen.hide();
-  },[]);
+  }, []);
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{
-        headerShown:false
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
         }}>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
         <Stack.Screen name="MainScreen" component={MainScreen} />
         <Stack.Screen name="AddScreen" component={AddScreen} />
       </Stack.Navigator>
---
+++ src/common/Input Text/text_input.tsx
@@ -0,0 +1,30 @@
+import {StyleProp, StyleSheet, Text, TextInput, TextStyle} from 'react-native';
+
+interface IInputText {
+  placeholder?: string;
+  onChangeText:(value:string)=>void;
+  secureText?:boolean;
+  
+  style?: StyleProp<TextStyle>;
+}
+
+const CustomInputText = ({placeholder,onChangeText ,style,secureText=false}: IInputText) => {
+  return <TextInput
+  placeholder={placeholder}
+  onChangeText={onChangeText}
+  secureTextEntry={secureText}
+  style={styles.textInput}
+/>;
+};
+
+const styles = StyleSheet.create({
+  textInput:{
+    borderWidth: 1,
+    borderRadius: 10,
+    fontSize: 20,
+    marginVertical: 8,
+    paddingHorizontal: 14,
+  }
+});
+
+export default CustomInputText;
